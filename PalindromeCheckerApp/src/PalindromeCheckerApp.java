037fc5a2d7432484405f257444b1d53c0027c3d7
anushkaraks <anushka.anchu@gmail.com>
2026-02-26 09:08:21 +0530

    Initial Commit

---
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+class U1PalindromeCheckerApp
+{
+    static final String AppName= "Palindrome Checker App";
+    static final String Version = "Version 1.0";
+    public static void main(String[] args)
+    {
+        System.out.println(" "+AppName);
+        System.out.println(" "+Version);
+        System.out.println("Welcome to the Palindrome Checker Application!");
+        System.out.println("This application will validate whether a given string is a palindrome.");
+        System.out.println(" ");
+        System.out.println("System initialized successfully.");
+        System.out.println("Ready to proceed to Palindrome Validation...");
+    }
+}
